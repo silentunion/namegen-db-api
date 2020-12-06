07bb1548088b83a286cff8cc69ee33c67cabf288
silentunion <milliner@unbc.ca>
2020-12-06 08:30:54 -0800

    Switched database from knex to pg for now/Moved old knex file

---
+++ src/database/database.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+const config = require('./config.json');
+
+exports.start = async function () {
+    const host = config.get('PGHOST');
+    const database = config.get('PGDATABASE');
+    const port = config.get('PGPORT');
+    const user = config.get('PGUSER');
+    const password = config.get('PGPASSWORD');
+
+    this.pool = new Pool({ host, database, port, user, password });
+};
+
+exports.close = async function () {
+    await this.pool.end();
+};
+
+exports.query = async function (q, data) {
+    return this.pool.query(q, data);
+};
