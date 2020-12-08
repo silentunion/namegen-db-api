0d9961e49f200f1c87bd14a00fae6245ca2ab16a
silentunion <milliner@unbc.ca>
2020-12-08 10:42:18 -0800

    Remove older files/removed knex

--- src/app/app.js
+++ src/app/app.js
@@ -2,11 +2,13 @@ const Koa = require('koa');
 const router = require('./../routes/routes');
 const ResponseTime = require('koa-response-time');
 const Morgan = require('koa-morgan');
+const cors = require('koa-cors');
 
 const db = require('./../database/database');
 
 const app = new Koa();
 
+app.use(cors());
 app.use(ResponseTime());
 app.use(Morgan('combined'));
 app.use(router.routes());
