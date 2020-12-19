b6d5bcf2a580134642574622d27fa3d3d493525f
silentunion <milliner@unbc.ca>
2020-12-19 12:56:35 -0800

    Further changes to the template function

--- sql/queries/template_functions.sql
+++ sql/queries/template_functions.sql
@@ -4,19 +4,22 @@ CREATE OR REPLACE FUNCTION insert_part(_new_part VARCHAR, _part_column VARCHAR, 
 RETURNS void AS
 $BODY$
 BEGIN 
-	IF NOT EXISTS 
-        (SELECT letter, cluster, syllable, stem, name
+    WITH get_parts as (
+        SELECT *
         FROM namegen.parts
-        FULL JOIN namegen.letters USING(part_id)
-        FULL JOIN namegen.clusters USING(part_id)
-        FULL JOIN namegen.syllables USING(part_id)
-        FULL JOIN namegen.stems USING(part_id)
-        FULL JOIN namegen.names USING(part_id)
+        FULL JOIN namegen.part_letters USING(part_id)
+        FULL JOIN namegen.part_clusters USING(part_id)
+        FULL JOIN namegen.part_syllables USING(part_id)
+        FULL JOIN namegen.part_stems USING(part_id)
+        FULL JOIN namegen.part_names USING(part_id)
         WHERE letter = _new_part
            OR cluster = _new_part
            OR syllable = _new_part
            OR stem = _new_part
-           OR name = _new_part)
+           OR name = _new_part
+        RETURNING *)
+	IF NOT EXISTS 
+        (SELECT * FROM get_parts)
     THEN
         WITH new_part_id AS (
             INSERT INTO namegen.parts (part_id) VALUES (DEFAULT)
@@ -30,17 +33,7 @@ BEGIN
          WHERE _part_column = _new_part)
     THEN
         INSERT INTO namegen._part_table (part_id, _part_column)
-            VALUES((SELECT part_id FROM namegen.parts
-                    JOIN namegen.letters USING(part_id)
-                    JOIN namegen.clusters USING(part_id)
-                    JOIN namegen.syllables USING(part_id)
-                    JOIN namegen.stems USING(part_id)
-                    JOIN namegen.names USING(part_id)
-                    WHERE letter = _new_part
-                        OR cluster = _new_part
-                        OR syllable = _new_part
-                        OR stem = _new_part
-                        OR name = _new_part), _new_part);
+            VALUES((SELECT part_id FROM get_parts), _new_part);
 	END IF
 END;
 $BODY$ LANGUAGE 'plpgsql';
