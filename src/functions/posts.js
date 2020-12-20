3da3cabc0feaf39315e388d53c668bc6ccbdf70a
silentunion <milliner@unbc.ca>
2020-12-20 10:54:00 -0800

    Changed if statement to insert parts  to new format

--- src/functions/posts.js
+++ src/functions/posts.js
@@ -12,22 +12,30 @@ exports.insert_parts = async function (req) {
 
         const part_exists = await selects.partExists(new_part);
 
-        // if part does not exist at all
-        if(!Array.isArray(part_exists) || !part_exists.length){
-
-            await inserts.insert_new_part(new_part, part_type, part_table);
-
+        if (!part_exists) {
+            await inserts.insert_new_part(new_part, category);
+        } else if (!part_type_exists) {
+            await inserts.add_category_to_part(new_part, category);
         } else {
-            const part_type_exists = await selects.partTypeExists(new_part, part_type, part_table);
-            // if part exists but part type does not
-            if (!Array.isArray(part_type_exists) || !part_type_exists.length){
-                const part_id = part_type_exists[0].part_id
-                
-                await inserts.insert_part_type(part_id, new_part, part_type, part_table);
+            console.log('Part already exists with category')
+        };  
 
-                console.log('Inserted new part');
-            };
-        };
+        // // if part does not exist at all
+        // if(!Array.isArray(part_exists) || !part_exists.length){
+
+        //     await inserts.insert_new_part(new_part, part_type, part_table);
+
+        // } else {
+        //     const part_type_exists = await selects.partTypeExists(new_part, part_type, part_table);
+        //     // if part exists but part type does not
+        //     if (!Array.isArray(part_type_exists) || !part_type_exists.length){
+        //         const part_id = part_type_exists[0].part_id
+                
+        //         await inserts.insert_part_type(part_id, new_part, part_type, part_table);
+
+        //         console.log('Inserted new part');
+        //     };
+        // };
 
     const ack = 'woohoo'
     return ack;
