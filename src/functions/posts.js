697527e96b73ad77a7f93169c9fb2986e5b3052d
silentunion <milliner@unbc.ca>
2020-12-20 12:08:01 -0800

    Fixed more bugs

--- src/functions/posts.js
+++ src/functions/posts.js
@@ -4,6 +4,7 @@ const inserts = require('../queries/inserts');
 
 exports.insert_parts = async function (req) {    
     var num_inserts = 0;
+    console.log(req);
 
     for (r of req) {
         console.log(r);
@@ -20,35 +21,17 @@ exports.insert_parts = async function (req) {
             console.log('New part inserted');
 
         } else {
-            const part_type_exists = await selects.partTypeExists(part);
+            const part_type_exists = await selects.partTypeExists(part, category);
             
             if (!part_type_exists) {
                 await inserts.add_category_to_part(part, category);
-                console.log('Existed part added to category');
+                console.log('Existing part added to category');
 
             } else {
                 console.log('Part already exists with category')
             }
         };
-  
-
-        // // if part does not exist at all
-        // if(!Array.isArray(part_exists) || !part_exists.length){
-
-        //     await inserts.insert_new_part(new_part, part_type, part_table);
-
-        // } else {
-        //     const part_type_exists = await selects.partTypeExists(new_part, part_type, part_table);
-        //     // if part exists but part type does not
-        //     if (!Array.isArray(part_type_exists) || !part_type_exists.length){
-        //         const part_id = part_type_exists[0].part_id
-                
-        //         await inserts.insert_part_type(part_id, new_part, part_type, part_table);
-
-        //         console.log('Inserted new part');
-        //     };
-        // };
-
+    };
     const ack = 'woohoo'
     return ack;
-}};
+};
