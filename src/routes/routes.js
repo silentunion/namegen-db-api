7f839f40fe0a0af85d2784ab64d63b8373399b91
silentunion <milliner@unbc.ca>
2020-12-20 07:16:40 -0800

    Created extra file to save unused queries/Renamed inserts folder

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ const Router = require('koa-router');
 const bodyParser = require('koa-body')();
 
 const database = require('./../database/database');
-const insert_parts = require('./../inserts/inserts')
+const insert_parts = require('./../queries/inserts')
 
 const router = Router();
 
