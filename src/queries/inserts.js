74346b590aed26035c15433a714e0df5ab235783
silentunion <milliner@unbc.ca>
2020-12-20 13:01:11 -0800

    Cleaned up code

--- src/queries/inserts.js
+++ src/queries/inserts.js
@@ -15,17 +15,11 @@ exports.insert_new_part = async function (part, category, collection, properties
     
     const col_id = await selects.getCollectionID(collection);
 
-    console.log('col_id_works')
-
     const cp_id = await database.query(`
         INSERT INTO namegen.collection_parts (col_id, part_id)
         VALUES (${col_id}, ${part_id})
             RETURNING cp_id;`)
         .then(res => res.rows[0].cp_id);
-    
-    console.log('cp_id_works')
-
-    console.log(properties)
 
     if (properties) {
         for (const [key, value] of Object.entries(properties)) {
@@ -37,9 +31,7 @@ exports.insert_new_part = async function (part, category, collection, properties
                     VALUES (${cp_id}, ${prop_id});`);
             };
         }
-    }
-    
-    console.log('props works')
+    };
 };
 
 exports.add_category_to_part = async function (new_part, category) {
