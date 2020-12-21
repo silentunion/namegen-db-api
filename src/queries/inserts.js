d0da63bc2c1d87a4c77b93879d752a6ee800e4f0
silentunion <milliner@unbc.ca>
2020-12-21 12:52:27 -0800

    Now accepts and implements properties

--- src/queries/inserts.js
+++ src/queries/inserts.js
@@ -40,37 +40,3 @@ exports.apply_properties_to_part = async function (part, category, collection, p
         console.log('Properties applied');
     };
 };
-
-    // await database.query(`
-    //     INSERT INTO namegen.part_categories (part_id, cat_id)
-    //     VALUES(${part_id}, ${cat_id});`);
-    
-    // const col_id = await selects.getCollectionID(collection);
-
-    // const cp_id = await database.query(`
-    //     INSERT INTO namegen.collection_parts (col_id, part_id)
-    //     VALUES (${col_id}, ${part_id})
-    //         RETURNING cp_id;`)
-    //     .then(res => res.rows[0].cp_id);
-
-    // if (properties) {
-    //     for (const [key, value] of Object.entries(properties)) {
-    //         if (value) {
-    //             prop_id = await selects.getPropertyID(key);
-
-    //             await database.query(`
-    //                 INSERT INTO namegen.part_properties (cp_id, prop_id)
-    //                 VALUES (${cp_id}, ${prop_id});`);
-    //         };
-    //     }
-    // };
-
-exports.insert_collection_parts = async function (collection_name, part_name) {
-    const col_id = await selects.getCollectionID(collection_name);
-    const part_id = await selects.getPartID(part_name);
-
-    await database.query(
-        `INSERT INTO namegen.collection_parts (col_id, part_id)
-         VALUES (${col_id}, ${part_id});`
-    );
-};
