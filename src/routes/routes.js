d814153fbacb09c2c268d9935321e0e5f9f1dd4c
silentunion <milliner@unbc.ca>
2020-12-31 11:27:47 -0800

    Changed starting endpoints to new system

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -18,28 +18,33 @@ router.get('/letters', async ctx => {
 
 router.get('/letters/freq', async ctx => {
     ctx.body = await database.query(
-        `SELECT part, frequency, property
+        `SELECT part, frequency, property, location
         FROM namegen.parts
         JOIN namegen.collection_parts USING(part_id)
         JOIN namegen.collections USING(col_id)
+        JOIN namegen.languages USING(lang_id)
+        JOIN namegen.themes USING(theme_id)
         JOIN namegen.part_properties USING(cp_id)
         JOIN namegen.properties USING(prop_id)
-        JOIN namegen.part_statistics USING(cp_id)
         WHERE category='letters'
-          AND collection='English Basic';`)
+          AND language='English US'
+          AND theme='Top 3000 Words';`)
     .then(c => c.rows)
     });
 
-router.get('/clusters', async ctx => {
+router.get('/clusters/freq', async ctx => {
     ctx.body = await database.query(
-        `SELECT part, property
+        `SELECT part, frequency, property, location
         FROM namegen.parts
         JOIN namegen.collection_parts USING(part_id)
         JOIN namegen.collections USING(col_id)
+        JOIN namegen.languages USING(lang_id)
+        JOIN namegen.themes USING(theme_id)
         JOIN namegen.part_properties USING(cp_id)
         JOIN namegen.properties USING(prop_id)
         WHERE category='clusters'
-          AND collection='English Basic';`)
+          AND language='English US'
+          AND theme='Top 3000 Words';`)
     .then(c => c.rows)
     });
 
