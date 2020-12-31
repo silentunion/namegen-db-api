ba96660264c7dddc262b24ba9ab4c1870334ae35
silentunion <milliner@unbc.ca>
2020-12-31 10:12:54 -0800

    Reset migrations/Updated schema

--- sql/migrations/00002_alter-enum.sql
+++
@@ -1,8 +0,0 @@
-ALTER TYPE namegen.enum_part_category RENAME VALUE 'letter' TO 'letters';
-ALTER TYPE namegen.enum_part_category RENAME VALUE 'cluster' TO 'clusters';
-ALTER TYPE namegen.enum_part_category RENAME VALUE 'syllable' TO 'syllables';
-ALTER TYPE namegen.enum_part_category RENAME VALUE 'stem' TO 'stems';
-ALTER TYPE namegen.enum_part_category RENAME VALUE 'name' TO 'names';
-
-ALTER TABLE namegen.languages RENAME COLUMN category TO class;
-ALTER TABLE namegen.themes RENAME COLUMN category TO topic;
